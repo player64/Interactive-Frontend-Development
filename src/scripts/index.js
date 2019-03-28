d6ff61243abb3c626f215a22b5fa5e74659687be
player64 <player.c64@gmail.com>
2019-03-28 22:50:14 +0000

    Rough autocomplete work added package to load google map through webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,36 @@
 import '../styles/index.scss';
+const loadGoogleMapsApi = require('load-google-maps-api');
 
-const initMap = () => {
-  console.log('fired');
-};
+/*
+    npm package through npm
+    https://www.npmjs.com/package/load-google-maps-api#usage
+ */
+loadGoogleMapsApi(
+    {
+        key: 'AIzaSyACc7KrutzJRAbkiTOAMuxM568LrJ8XOjE',
+        libraries: ['places']
+    }
+).then(function (googleMaps) {
+    const map = new googleMaps.Map(document.querySelector('.map'), {
+        center: {
+            lat: 40.7484405,
+            lng: -73.9944191
+        },
+        zoom: 12
+    });
+    const countryRestrict = {'country': 'us'};
+    const autocomplete = new googleMaps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */ (
+            document.getElementById('autocomplete')), {
+            types: ['(cities)'],
+            componentRestrictions: countryRestrict
+        });
+    const places = new google.maps.places.PlacesService(map);
+    autocomplete.addListener('place_changed', onPlaceChanged);
+
+    const onPlaceChanged = () => {
+      console.log('fired on place changed');
+    };
+}).catch(function (error) {
+    console.error(error);
+});
