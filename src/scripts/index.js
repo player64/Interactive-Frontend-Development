bdcf9429afb24c72fe21c2f99bf3f9128dda80b1
player64 <player.c64@gmail.com>
2019-03-31 16:56:34 +0100

    Markers added to the map

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,7 @@ const loadGoogleMapsApi = require('load-google-maps-api');
 
 loadGoogleMapsApi(
     {
-        key: 'AIzaSyACc7KrutzJRAbkiTOAMuxM568LrJ8XOjE',
+        key: '',
         libraries: ['places']
     }
 ).then((googleMaps) => {
@@ -22,13 +22,56 @@ loadGoogleMapsApi(
     let autocomplete;
     let places;
     let getNextPage = null;
-    const markers = [];
+    let selectedPlaceType = null;
+    let placeMarker = null;
+    let visibleMap = false;
+
+    // place type definition
+    const placeType = {
+        bars: {
+            types: ['food', 'restaurant', 'bar'],
+            icon: '/public/1x/map-restaurant.png'
+        },
+        hotels: {
+            types: ['lodging'],
+            icon: '/public/1x/map-hotels.png'
+        },
+        attr: {
+            types: ['museum', 'art_gallery', 'aquarium', 'stadium'],
+            icon: '/public/1x/map-attractions.png'
+        }
+    };
+    let markers = [];
     const hostnameRegexp = new RegExp('^https?://.+?/');
 
     // load more results on click
     $('#more').click(() =>{
         selectors.moreBtn.disabled = true;
         if (getNextPage) getNextPage();
+    });
+
+    // app navigation
+    $('.appNav button').click(function() {
+        const _this = $(this);
+        const parentList = _this.parent().parent();
+        const parentLi = _this.parent();
+        const target = _this.attr('data-target');
+
+        if (parentLi.hasClass('active') || !(target in placeType)) {
+            return false;
+        }
+
+        selectedPlaceType = placeType[target].types;
+        placeMarker = placeType[target].icon;
+
+        if (visibleMap) {
+            clearMarkers();
+            search();
+        }
+
+        parentList.find('li').removeClass('active');
+        parentLi.addClass('active');
+
     });
 
     function initMap() {
@@ -55,6 +98,8 @@ loadGoogleMapsApi(
     const onPlaceChanged = () => {
         const place = autocomplete.getPlace();
         if (place.geometry) {
+            visibleMap = true;
+            clearMarkers();
             // set the map position
             map.panTo(place.geometry.location);
             map.setZoom(15);
@@ -67,22 +112,56 @@ loadGoogleMapsApi(
     };
 
     const search = () => {
+        // check is types empty
+        if (!selectedPlaceType) {
+            selectedPlaceType =  placeType['hotels'].types;
+        }
+
+        if (!placeMarker) {
+            placeMarker = placeType['hotels'].icon;
+        }
         // search configuration
         const search = {
             bounds: map.getBounds(),
-            types: ['lodging']
+            types: selectedPlaceType
         };
 
         places.nearbySearch(search, function(results, status, pagination)  {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
+            if (status === googleMaps.places.PlacesServiceStatus.OK) {
+                let key = (markers.length) ? markers.length : 0;
+                let c = 0;
+                results.forEach((item) => {
+                    markers[key] = new googleMaps.Marker({
+                        position: item.geometry.location,
+                        animation: googleMaps.Animation.DROP,
+                        icon: placeMarker
+                    });
+                    setTimeout(dropMarker(key), c  * 100);
+                    key+=1;
+                    c+=1;
+                });
                 // add more results if any
                 selectors.moreBtn.disabled = !pagination.hasNextPage;
                 getNextPage = pagination.hasNextPage && function() {
                     pagination.nextPage();
                 };
-                console.log(results);
             }
         });
+    };
+
+    const clearMarkers = () => {
+        for (let i = 0; i < markers.length; i++) {
+            if (markers[i]) {
+                markers[i].setMap(null);
+            }
+        }
+        markers = [];
+    };
+
+    const dropMarker = (key) => {
+        return () => {
+            markers[key].setMap(map);
+        };
     };
 
     // fire the app
